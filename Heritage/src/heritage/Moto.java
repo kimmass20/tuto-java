1c5283a80b9a99ddeec572636b9c021485d7f6f8
ELIAKIM MASSAMBA <massk0170@gmail.com>
2026-01-06 21:21:48 +0100

    notions d'heritage

---
+++ Heritage/src/heritage/Moto.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package heritage;
+
+/**
+ *
+ * @author massk
+ */
+public class Moto {
+    private int NbrRoue;
+    
+    public Moto (String marque, int NbrRoue){
+        super (marque);
+        this.NbrRoue = NbrRoue;
+    }
+}
---
+++ Heritage/src/heritage/Vehicule.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package heritage;
+
+/**
+ *
+ * @author massk
+ */
+public class Vehicule {
+    private String marque;
+    
+    public Vehicule (String marque){
+        this.marque = marque;
+    }
+}
